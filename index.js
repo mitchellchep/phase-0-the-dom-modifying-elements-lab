a826f54a736181468decffbab0572fc41af6c3cd
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-10-17 08:22:25 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the main#main element
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new h1 element to 'victory'
+newHeader.id = 'victory';
+
+newHeader.textContent = 'MITCHELL-CHEPKOECH is the champion';
+// Append new h1 element to the body
+document.body.appendChild(newHeader);
